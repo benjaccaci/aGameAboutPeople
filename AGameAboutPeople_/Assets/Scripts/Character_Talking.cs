93e613cdd3a75fe918a317f692c2a917fb392ecb
aulin1 <audrey.youran.lin@gmail.com>
2024-11-09 15:28:57 -0500

    Added sprites

--- AGameAboutPeople_/Assets/Scripts/Character_Talking.cs
+++ AGameAboutPeople_/Assets/Scripts/Character_Talking.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Character_Talking : MonoBehaviour
 {
+    private int current_state; //0 if not talked to, 1 if success, -1 if failure
+
+    [SerializeField]
+    private string talking_scene;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        current_state = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void interacted_with(){
+        if(current_state == 0){
+            Debug.Log(talking_scene);
+            Debug.Log("Should start here");
+        } else if(current_state == 1){
+            Debug.Log("You've already succeeded.");
+        } else {
+            Debug.Log("You've already failed.");
+        }
     }
 }
